6dc4a5bc62bc0385712f471ac7831bb96b243e1c
xcWhy <78636566+xcWhy@users.noreply.github.com>
2025-06-27 17:38:05 +0300

    Delete Models directory

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CustomIdentityWeb.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
